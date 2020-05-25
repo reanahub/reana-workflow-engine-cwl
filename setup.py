8ddc791170f5890e34c00be92c81fbcac31f394e
Tibor Šimko <tibor.simko@cern.ch>
2020-05-25 15:46:42 +0200

    installation: bump reana-commons
    Signed-off-by: Tibor Šimko <tibor.simko@cern.ch>

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ install_requires = [
     'enum34>=1.1.6',
     'SQLAlchemy>=1.3.7',
     'SQLAlchemy-Utils>=0.34.2',
-    'reana-commons>=0.6.0,<0.7.0',
+    'reana-commons>=0.6.1,<0.7.0',
 ]
 
 packages = find_packages()
