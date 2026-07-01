d919b47e8c1159cc9146987f63f965fc9b669a2e
benoit.roland <benoit.roland@kit.edu>
2026-07-01 14:18:52 +0200

    feat: gpu, notification option, email address (base latest master)

--- reana_workflow_engine_cwl/cwl_reana.py
+++ reana_workflow_engine_cwl/cwl_reana.py
@@ -308,7 +308,10 @@ class ReanaPipelineJob(JobBase):
         kubernetes_memory_limit = self._get_hint("kubernetes_memory_limit")
         kubernetes_job_timeout = self._get_hint("kubernetes_job_timeout")
         c4p_cpu_cores = self._get_hint("c4p_cpu_cores")
+        c4p_request_gpus = self._get_hint("c4p_request_gpus")
         c4p_memory_limit = self._get_hint("c4p_memory_limit")
+        c4p_notification = self._get_hint("c4p_notification")
+        c4p_email_address = self._get_hint("c4p_email_address")
         c4p_additional_requirements = self._get_hint("c4p_additional_requirements")
         create_body = {
             "image": container,
@@ -338,7 +341,10 @@ class ReanaPipelineJob(JobBase):
             "slurm_partition": slurm_partition,
             "slurm_time": slurm_time,
             "c4p_cpu_cores": c4p_cpu_cores,
+            "c4p_request_gpus": c4p_request_gpus,
             "c4p_memory_limit": c4p_memory_limit,
+            "c4p_notification": c4p_notification,
+            "c4p_email_address": c4p_email_address,
             "c4p_additional_requirements": c4p_additional_requirements,
         }
 
